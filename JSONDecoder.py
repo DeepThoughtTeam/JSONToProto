e797bceb28ce7dfaf05ddf47052cb8499ab5e078
amaliujia <amaliujia@163.com>
2015-10-28 15:59:44 -0400

    progress 1

---
+++ JSONDecoder.py
@@ -0,0 +1,67 @@
+import simplejson as json
+
+class JSONDecoder:
+  def decodeFromFile(self, file_path, output_path):
+    json_data=open("data/jsonsample.json").read()
+    caffe_data = self.decode(json_data)
+    open(output_path,'w').write(caffe_data)
+
+  def decode(self, j=''):
+    if len(j) == 0:
+      return
+    data = json.loads(j)
+    return self.decodeDictToStr(data, 0)
+
+  def decodPairToStr(self, key, value):
+    res = ""
+    res += str(key)
+    res += ":"
+    if isinstance(value, str) and value != "MAX":
+      res += "\"" + value +"\""
+    else:
+      res += str(value)
+    res +="\n"
+    return res
+
+  def decodeValueToStr(self, value):
+    res = ""
+    if isinstance(value, str) and value != "MAX":
+      res += "\"" + value +"\""
+    else:
+      res += str(value)
+    res +="\n"
+    return res
+
+  def decodeDictToStr(self, dictionary, tab):
+    tabs = "  " * tab
+    res = "{\n"
+    for key,value in dictionary.iteritems():
+
+      if isinstance(value, dict):
+        res += tabs
+        res += key
+        res += ":"
+        res += self.decodeDictToStr(value, tab + 1);
+      elif isinstance(value, list): # assume list only contains dicts
+        for element in value:
+          res += tabs
+          res += key
+          res += ":"
+          if isinstance(element, dict):
+            res += self.decodeDictToStr(element, tab + 1)
+          else:
+            res += self.decodeValueToStr(element)
+      else:
+        res += tabs
+        res += self.decodPairToStr(key, value)
+    res += tabs
+    res += "}\n"
+    return res
+
+#decoder = JSONDecoder()
+#decoder.decode('{"name":"mnist_siamese","input":"data","input_shape":{"dim":10000,"dim":1,"dim":28,"dim":28}}')
+#obj = json.loads('{"name":"mnist_siamese","input":"data","input_shape":{"dim":10000,"dim":1,"dim":28,"dim":28}}')
+#type(obj)
+
+decoder = JSONDecoder()
+decoder.decodeFromFile("data/jsonsample.json", "data/jsonsample.prototxt")
